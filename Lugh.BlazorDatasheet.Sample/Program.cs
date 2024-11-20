7f1a2a6c208f2347f1b853b9f7661975c162d9fb
AndyBevan <7716304+AndyBevan@users.noreply.github.com>
2024-11-19 22:56:32 -0500

    BlazorDatasheet sample added

--- Lugh.BlazorDatasheet.Sample/Program.cs
+++ Lugh.BlazorDatasheet.Sample/Program.cs
@@ -1,5 +1,6 @@
 using Lugh.BlazorDatasheet.Sample.Client.Pages;
 using Lugh.BlazorDatasheet.Sample.Components;
+using BlazorDatasheet.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddBlazorDatasheet();
 
 var app = builder.Build();
 
